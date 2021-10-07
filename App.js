b41b2b6291c8312f57b580bbc6cb4b7063ff3131
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2021-10-07 13:42:01 +0400

    image with fade effect and style

--- App.js
+++ App.js
@@ -4,12 +4,23 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("text pressed")
+  console.log(require("./assets/favicon.png"));
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World! Kirty mah love !😍</Text>
-      <Text numberOfLines={1} onPress={handlePress}>Amazing First App</Text>
+      <Text style={styles.textstuff}>
+        Hello World! Kirty mah love !😍
+      </Text>
+      <Text style={styles.textstuff} numberOfLines={1} onPress={handlePress}>Amazing First App</Text>
       <Image source={require("./assets/favicon.png")} />
+      <Image 
+        blurRadius={1}
+        source={{ 
+          uri: "https://picsum.photos/200/300" ,
+          width: 200,
+          height: 300,
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -22,4 +33,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textstuff: {
+    color: '#fff'
+  }
 });
