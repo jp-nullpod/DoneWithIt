c0575fbd443c24ab212ca4634ab4b9067a37300e
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2021-10-08 16:52:16 +0400

    adding hooks

--- App.js
+++ App.js
@@ -3,57 +3,22 @@ import {
   StyleSheet, Text, Image, 
   SafeAreaView, View, Alert, 
   TouchableOpacity, TouchableHighlight, Button,
-  StatusBar, Platform } from 'react-native';
+  StatusBar, Platform, Dimensions } from 'react-native';
+
+import { useDimensions } from "@react-native-community/hooks";
 
 export default function App() {
   const handlePress = () => console.log("element pressed")
   const containerStyle = { backgroundColor: "white" }
+  console.log(useDimensions())
 
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-       <Button 
-        color="blue" 
-        title="Top Button" 
-        onPress={()=> Alert.alert(
-          "My Title", 
-          "My Message",
-          [{ text: "yes"}, {text: "no"}, {text: "dont know"}]
-          )
-        }/>
-      <Text style={styles.textstuff}>
-        Hello World! Kirty mah love !😍
-      </Text>
-      <Text style={styles.textstuff} numberOfLines={1} onPress={handlePress}>Amazing First App</Text>
-      
-      <Image source={require("./assets/favicon.png")} />
-      <TouchableOpacity onPress={handlePress}>
-        <View style={{ width:100, height: 70, backgroundColor: 'dodgerblue' }}>
-            
-        </View>
-      </TouchableOpacity>
-      <Button 
-        color="orange" 
-        title="Click Me" 
-        onPress={()=> Alert.alert("My Title", "My Message", [
-          { text: "Yes", onPress: ()=> console.log("Yes") },
-          { text: "No", onPress: ()=> console.log("No")}
-        ])
-        }/>
-
-        <Button 
-        color="blue" 
-        title="Prompt Me" 
-        onPress={()=> Alert.prompt("My Title", "My Message", text => console.log(text) )
-        }/>
-      <TouchableHighlight onPress={handlePress}>
-          <Image 
-            source={{ 
-              uri: "https://picsum.photos/200/300" ,
-              width: 200,
-              height: 300,
-            }}
-          /> 
-      </TouchableHighlight>
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: '100%',
+        height: '30%'
+      }}></View>
 
     </SafeAreaView>
   );
@@ -62,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     paddingTop: Platform.OS === 'android'? StatusBar.currentHeight : 0
   },
   textstuff: {
