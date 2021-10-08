38ce7311d2a9558fd107195e4c45007e0b0a8301
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2021-10-08 15:03:41 +0400

    using other component types

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, Image, SafeAreaView, View, Alert, TouchableOpacity, TouchableHighlight, Button } from 'react-native';
 
 export default function App() {
-  const handlePress = () => console.log("text pressed")
-  console.log(require("./assets/favicon.png"));
+  const handlePress = () => console.log("element pressed")
 
   return (
     <SafeAreaView style={styles.container}>
@@ -12,15 +11,36 @@ export default function App() {
         Hello World! Kirty mah love !😍
       </Text>
       <Text style={styles.textstuff} numberOfLines={1} onPress={handlePress}>Amazing First App</Text>
+      
       <Image source={require("./assets/favicon.png")} />
-      <Image 
-        blurRadius={1}
-        source={{ 
-          uri: "https://picsum.photos/200/300" ,
-          width: 200,
-          height: 300,
-        }}
-      />
+      <TouchableOpacity onPress={handlePress}>
+        <View style={{ width:100, height: 70, backgroundColor: 'dodgerblue' }}>
+            
+        </View>
+      </TouchableOpacity>
+      <Button 
+        color="orange" 
+        title="Click Me" 
+        onPress={()=> Alert.alert("My Title", "My Message", [
+          { text: "Yes", onPress: ()=> console.log("Yes") },
+          { text: "No", onPress: ()=> console.log("No")}
+        ])
+        }/>
+
+        <Button 
+        color="blue" 
+        title="Prompt Me" 
+        onPress={()=> Alert.prompt("My Title", "My Message", text => console.log(text) )
+        }/>
+      <TouchableHighlight onPress={handlePress}>
+          <Image 
+            source={{ 
+              uri: "https://picsum.photos/200/300" ,
+              width: 200,
+              height: 300,
+            }}
+          /> 
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -29,11 +49,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   textstuff: {
-    color: '#fff'
+    color: '#000'
   }
 });
